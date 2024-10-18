ffe1f100e17d9a2ebc1a72743916b43d6d6c3191
maryam-ergen <maryam.ergen@mths.ca>
2024-10-18 11:12:23 -0400

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,85 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Oct 2024
+ * This program lights up neopixels according to light level
 */
 
-basic.showString('Hello, World!')
+//variables
+let lightAmount: number
+let neopixelStrip: neopixel.Strip = null
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//cleanup
+input.onButtonPressed(Button.A, function () {
+    basic.pause(1000)
+    basic.clearScreen()
+    lightAmount = input.lightLevel()
+
+
+//when light level is less than 51
+    if (lightAmount <= 51) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+        
+}
+
+//when light level is equal or more than 52
+    if (lightAmount > 52) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.White))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+}
+
+//when light level is equal or more than 104
+    if (lightAmount > 104) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Purple))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+}
+
+//when light level is equal or more than 156
+    if (lightAmount > 156) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.pause(1000)
+}
+
+//when light level is equal or more than 208
+    if (lightAmount > 208) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(1000)
+}
+
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    basic.showIcon(IconNames.Happy)
+})
